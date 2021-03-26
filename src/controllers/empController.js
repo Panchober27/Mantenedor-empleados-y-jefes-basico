fdc056776bab273812f3812ee1b93e040a37214d
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2021-03-26 00:24:56 -0300

    Ordenando un poquito😋😋

--- src/controllers/empController.js
+++ src/controllers/empController.js
@@ -1,11 +1,12 @@
 // Arvhico que manejara las consultas a las bases de datos.
 const empController = {};
+
+// Requiriendo la base de datos jiji 😋
 const pool = require("../database");
 
 empController.list = async (req, res) => {
   const emps = await pool.query("SELECT * FROM empleado");
   console.log(emps);
-  //   res.send(emps);
   res.render("empleados/list", { emps });
 };
 
--- src/routes/empleados.js
+++ src/routes/empleados.js
@@ -4,8 +4,6 @@ const router = express.Router();
 // Controlador de los empleados.
 const empController = require("../controllers/empController");
 
-// Conexion a la bd.
-const pool = require("../database");
 
 // Ruta al listado de los empleados.
 router.get("/list-emp", empController.list);
